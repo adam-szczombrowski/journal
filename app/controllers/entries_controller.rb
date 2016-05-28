2a2b0e2d6a234ff774f478176d32afedd42556b1
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2016-05-28 18:44:25 +0200

    add entry model and controller

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,52 @@
+class EntriesController < ApplicationController
+  before_action :find_entry, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @entries = Entry.all
+  end
+
+  def show
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    @entry.user_id = current_user.id
+    if @entry.save
+      redirect_to @entry
+    else
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @entry.update_attributes(entry_params)
+    if @entry.save
+      redirect_to @entry
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  def destroy
+    @entry.destroy
+    redirect_to entries_path
+  end
+
+  private
+
+  def find_entry
+    @entry = Entry.find(params[:id])
+  end
+
+  def entry_params
+    params.require(:entry).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#welcome'
 
   devise_for :users
-  
+  resources :entries
 end
